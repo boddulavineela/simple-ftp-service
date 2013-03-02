42eecb09c3fac92cf248f40712ff56dfcd67f363
sau2pen@gmail.com <sau2pen@gmail.com@546bf3ba-b6f0-9339-24fa-284c60899f40>
2013-03-02 22:37:37 +0000

    Implemented method to read file and split them into Segments.

--- simpleFTP/src/edu/ncsu/csc573/project2/client/Client.java
+++ simpleFTP/src/edu/ncsu/csc573/project2/client/Client.java
@@ -4,7 +4,17 @@
  */
 package edu.ncsu.csc573.project2.client;
 
+import edu.ncsu.csc573.project2.util.Constants;
+import edu.ncsu.csc573.project2.util.Segment;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.LinkedList;
 
 /**
  *
@@ -19,7 +29,7 @@ public class Client {
     DatagramSocket clientSocket;    //The client side datagram socket
     String fileContents;    //String to store the file contents
     byte sendBuffer[];      //The send buffer
-    byte receiveBuffer[];   //The receive buffer
+    byte recvBuffer[];   //The receive buffer
     
     public Client(String serverHostName, int serverPortNumber,
                   String fileName, int n, int mss) {
@@ -29,9 +39,58 @@ public class Client {
         this.n = n;
         this.mss = mss;
 
-        //Client operation code
+        sendBuffer = new byte[mss];
+        recvBuffer = new byte[n * mss];
+        try {
+            clientSocket = new DatagramSocket();
+        } catch (Exception e) {
+            System.err.println("Failed to create client datagram socket");
+        }
+          //Client operation code
+        LinkedList<Segment> segments = readFileContentsAsSegments();
+
+        for (int i = 0; i < segments.size(); ++i) {
+            System.out.println(segments.get(i).toString());
+        }
+        //sendData();
     }
-    
+   
+    public LinkedList<Segment> readFileContentsAsSegments() {
+        LinkedList<Segment> segments = new LinkedList<Segment>();
+        String content = "";
+        try {
+            BufferedReader buf = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fileName))));
+            String currentLine;
+            while ((currentLine = buf.readLine()) != null) {
+                content += currentLine + "\n";
+            }
+
+            ByteArrayInputStream bis = new ByteArrayInputStream(content.getBytes());
+            int len = content.length();
+            while (len > 0) {
+                byte segmentData[] = new byte[mss - Constants.kSegmentHeaderSize];
+                bis.read(segmentData, 0, segmentData.length);
+                Segment currentSegment = new Segment(mss, Constants.kDatatype, segmentData);
+                segments.add(currentSegment);   
+                len -= segmentData.length;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return segments; 
+    }
+    public void sendData() {
+         try {
+            InetAddress IPAddress = InetAddress.getByName(Constants.kServerHostName);
+            DatagramPacket sendPacket = new DatagramPacket("hello".getBytes(), 4, IPAddress, Constants.kServerPortNumber);
+            clientSocket.send(sendPacket);
+            clientSocket.close();
+        } catch (Exception e) {
+            
+        }
+     
+    }
     public static void main(String[] args) {
         if (args.length != 5) {
             System.err.println("Usage : java Client <server-host-name> <server-port#> " + 
---
+++ simpleFTP/src/edu/ncsu/csc573/project2/util/Constants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ncsu.csc573.project2.util;
+
+/**
+ *
+ * @author svpendse1
+ */
+public class Constants {
+    public static final int kMaxBufferSize = 65535;
+    public static final int kServerPortNumber = 7735;
+    public static final int kSegmentHeaderSize = 8;
+    
+    public static String kServerHostName = "127.0.0.1";
+
+    public static final char kAckType = 43690;
+    public static final char kDatatype = 21845;
+}
