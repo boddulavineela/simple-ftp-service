380f7e7f24df8cdd14e4f99ae43c8aa526d418fa
sau2pen@gmail.com <sau2pen@gmail.com@546bf3ba-b6f0-9339-24fa-284c60899f40>
2013-03-04 03:35:08 +0000

    Added checksum calculation. Resolved several bugs with tedious bitwise operations. Hopefully, error free now!

--- trunk/simpleFTP/src/edu/ncsu/csc573/project2/util/Segment.java
+++ trunk/simpleFTP/src/edu/ncsu/csc573/project2/util/Segment.java
@@ -8,6 +8,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.InetAddress;
 import java.util.Scanner;
 
 /**
@@ -34,18 +35,84 @@ public class Segment {
     public Segment(int sequence_number, char segmentType, char checksum, byte data[]) {
         //this.mss = mss;
         this.data = data;
+        this.acknowledged = false;
         /*if (data.length > (mss - Constants.kSegmentHeaderSize)) {
             System.err.println("Segment size cannot exceed " + mss + " bytes");
         }*/
         header = new SegmentHeader(sequence_number, checksum, segmentType);
 
     }
+
+    public SegmentHeader getHeader() {
+        return header;
+    }
+
+    public void setHeader(SegmentHeader header) {
+        this.header = header;
+    }
+    
+    
+    public boolean isAcknowledged() {
+        return this.acknowledged;
+    }
+
+    public void setAcknowledged(boolean acknowledged) {
+        this.acknowledged = true;
+    }
     
     public void setData(byte data[]) {
         this.data = data;
     }
     
-    class SegmentHeader {
+    public byte[] getData() {
+        return data;
+    }
+   
+    public static char calculateChecksum(Segment segment, byte sourceAddress[], byte destAddress[]) {
+   
+        byte segment_data[] = segment.getData(); 
+        int pad_length = (int)Math.ceil(1.0 * segment_data.length / 4);
+        byte segment_data_padded[] = new byte[4 * pad_length];
+        for (int i = 0; i < segment_data.length; ++i) {
+            segment_data_padded[i] = segment_data[i];
+        }
+       
+        int result = 0;
+        //Pseudo Header
+        result += (sourceAddress[0] << 8 | sourceAddress[1]);
+        result += (sourceAddress[2] << 8 | sourceAddress[3]);
+        result += (destAddress[0] << 8 | destAddress[1]);
+        result += (destAddress[2] << 8 | destAddress[3]);
+        result += (0x0011);
+        int total_length = segment.getSegment().length;
+        result += (total_length & 0xFFFF);
+        
+        //Header
+        int sequence_number = segment.getHeader().getSequence_number();
+        int segmentType = segment.getHeader().getSegmentType();
+        int checksum = segment.getHeader().getChecksum();
+        result += (sequence_number >> 16) + (sequence_number & 0xFFFF) + segmentType; 
+
+        /*System.out.println("Seq Low : " + (sequence_number >> 16));
+        System.out.println("Seq High : " + (sequence_number & 0xFFFF));
+        System.out.println("Segment Type : " + (int)segmentType);
+        System.out.println("Checksum : " + checksum);
+        System.out.println("Result before data : " + result);*/
+
+        //Data
+        for (int i = 0; i < segment_data_padded.length; i+=4) {
+            result += (segment_data_padded[i] << 8 | segment_data_padded[i+1]);
+            result += (segment_data_padded[i+2] << 8 | segment_data_padded[i+3]);
+        }
+        
+        //System.out.println("Result after data : " + result);
+        int carry = result >> 16;
+        result += carry;
+        
+        return (char)result;
+    }
+    
+    public class SegmentHeader {
         int sequence_number;    //A 32-bit sequence number;
         char checksum;         //The checksum value
         char segmentType;       //The packet type
@@ -54,24 +121,50 @@ public class Segment {
             this.checksum = checksum;
             this.segmentType = segmentType;
         }
-       
-        public String toString() {
-            return "Seq : " + sequence_number + ", Checksum : " + checksum + ", SegType : " + segmentType; 
+
+        public int getSequence_number() {
+            return sequence_number;
         }
 
-        public String getSegmentHeader() {
+        public void setSequence_number(int sequence_number) {
+            this.sequence_number = sequence_number;
+        }
+
+        public char getChecksum() {
+            return checksum;
+        }
+
+        public void setChecksum(char checksum) {
+            this.checksum = checksum;
+        }
+
+        public char getSegmentType() {
+            return segmentType;
+        }
+
+        public void setSegmentType(char segmentType) {
+            this.segmentType = segmentType;
+        }
+      
+        
+        public String toString() {
+            return "Seq : " + sequence_number + ", Checksum : " + (int)checksum + ", SegType : " + (int)segmentType; 
+        }
+
+        public byte[] getSegmentHeader() {
             try {
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 DataOutputStream dos = new DataOutputStream(bos);
                 dos.writeInt(sequence_number);
                 dos.writeChar(checksum);
                 dos.writeChar(segmentType);
-                dos.close();
                 byte header_bytes[] = bos.toByteArray();
+                dos.close();
                 bos.close();
 
-                return new String(header_bytes);
+                return header_bytes;
             } catch (Exception e) {
+                e.printStackTrace();
             }
             return null;
         }
@@ -81,18 +174,27 @@ public class Segment {
         return "" + "(" + header.toString() + ") " + ", Data = " + new String(data);
     }
 
-    public String getSegment() {
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(header.getSegmentHeader());
-        if (data != null) {
-            buffer.append(new String(data));
+    public byte[] getSegment() {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            DataOutputStream dos = new DataOutputStream(bos);
+            dos.write(header.getSegmentHeader());
+            if (data != null) {
+                dos.write(data);
+            }
+            byte segment_bytes[] = bos.toByteArray();
+            dos.close();
+            bos.close();
+            return segment_bytes;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return buffer.toString(); 
+        return null;
     }
 
-    public static Segment parseFromString(String input) { 
+    public static Segment parseFromBytes(byte input[]) { 
         try {
-            ByteArrayInputStream bis = new ByteArrayInputStream(input.getBytes());
+            ByteArrayInputStream bis = new ByteArrayInputStream(input);
             DataInputStream dis = new DataInputStream(bis);
             int seq = dis.readInt();
             char cksum = dis.readChar(); 
@@ -111,5 +213,34 @@ public class Segment {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Segment segment = new Segment(1, Constants.kDatatype, (char)0, "hell hell hello".getBytes());
+        char checksum = Segment.calculateChecksum(segment, InetAddress.getLocalHost().getAddress(), InetAddress.getLocalHost().getAddress());
+        byte segment_bytes[] = segment.getSegment();
+        for (int i = 0; i < segment_bytes.length; ++i) {
+            System.out.print(segment_bytes[i] + " " );
+        }
+        System.out.println();
+        segment.getHeader().setChecksum((char)32866);
+
+        segment_bytes = segment.getSegment();
+        for (int i = 0; i < segment_bytes.length; ++i) {
+            System.out.print(segment_bytes[i] + " " );
+        }
+        System.out.println();
+        System.out.println("Final Checksum : " + (int)checksum);
+        
+        Segment parsedSegment = Segment.parseFromBytes(segment.getSegment());
+        segment_bytes = parsedSegment.getSegment();
+        for (int i = 0; i < segment_bytes.length; ++i) {
+            System.out.print(segment_bytes[i] + " " );
+        }
+        System.out.println();
+        checksum = Segment.calculateChecksum(parsedSegment, InetAddress.getLocalHost().getAddress(), InetAddress.getLocalHost().getAddress());
+        System.out.println("Final Checksum : " + (int)checksum);
+        //segment.getHeader().setChecksum((char)((~checksum) & 0xFFFF));
+        //System.out.println("Checksum : " + (int)Segment.calculateChecksum(segment, InetAddress.getLocalHost().getAddress(), InetAddress.getLocalHost().getAddress()));
     }
 }
