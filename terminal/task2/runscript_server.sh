e6851e14b12c22c8da357b1ec5faf461237512c5
sau2pen@gmail.com <sau2pen@gmail.com@546bf3ba-b6f0-9339-24fa-284c60899f40>
2013-04-10 22:41:33 +0000

    - modified README - modified server scripts to take file name as a parameter - modifiled client scripts to take server ip as a parameter

--- terminal/task2/runscript_server.sh
+++ terminal/task2/runscript_server.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-
+#$1 - target file name
+#$2 - method (optional)
 #if there are no input arguments, use the Go Back N method, otherwise use the specified method.
-if [ $# -eq 0 ] 
-  then
-    java -classpath ../bin edu.ncsu.csc573.project2.server.Server 7735 /Users/svpendse1/Desktop/rfc_transfer.txt 0.05 0
+if [ $# -eq 0 ]
+  then 
+    echo "Usage : sh runscript_server.sh <filename> [<method>] (0: GoBackN, 1: SelRepeat)"
 else 
-    java -classpath ../bin edu.ncsu.csc573.project2.server.Server 7735 /Users/svpendse1/Desktop/rfc_transfer.txt 0.05 $1
+    if [ $# -eq 1 ] 
+      then
+        java -classpath ../bin edu.ncsu.csc573.project2.server.Server 7735 $1 0.05 0
+    else 
+        java -classpath ../bin edu.ncsu.csc573.project2.server.Server 7735 $1 0.05 $2
+    fi
 fi
