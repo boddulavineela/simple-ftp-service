ca56f253165aebeaebcedd08ad89aadb47176fab
sau2pen@gmail.com <sau2pen@gmail.com@546bf3ba-b6f0-9339-24fa-284c60899f40>
2013-04-11 02:52:57 +0000

    - modified client scripts to replace method code with method name in output result files. - changed the matlab script and README accordingly.

--- terminal/task3/archive/parsescript.sh
+++ terminal/task3/archive/parsescript.sh
@@ -1,10 +1,14 @@
-if [ $# -eq 0 ] 
-   then 
-        echo "Please specify method : 0 - GoBackN, 1 - SelRepeat"
-else
-    rm -f results_task3_$1
-    for f in ./temp_$1* 
-    do
-        cat $f | awk '{sum+=$9; sumsq+=$9*$9} END {print sum/NR, sum/NR - 2.776 * sqrt(sumsq/NR - (sum/NR)**2) / sqrt(NR), sum/NR + 2.776 * sqrt(sumsq/NR - (sum/NR)**2)/ sqrt(NR)}' >> results_task3_$1
-    done
+#!/bin/sh
+
+method_name="gobackn"
+if [ $1 -eq 1 ] 
+  then
+    method_name="selrepeat"
 fi
+
+rm -f results_task3_$method_name.txt
+
+for f in ./temp_$1* 
+do
+    cat $f | awk '{sum+=$9; sumsq+=$9*$9} END {print sum/NR, sum/NR - 2.776 * sqrt(sumsq/NR - (sum/NR)**2) / sqrt(NR), sum/NR + 2.776 * sqrt(sumsq/NR - (sum/NR)**2)/ sqrt(NR)}' >> results_task3_$method_name.txt
+done
